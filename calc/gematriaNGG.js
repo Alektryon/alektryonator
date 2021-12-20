7399390092524a8e29a6a3591c623d47322db165
Alektryon <84470810+Alektryon@users.noreply.github.com>
2021-12-20 11:57:03 +0000

    Update gematriaNGG.js

--- calc/gematriaNGG.js
+++ calc/gematriaNGG.js
@@ -1,6 +1,6 @@
 var catArr = []; gemArr = []
 var cipherArray = [];
-var openCiphers = ["Bacon Simple", "Bacon Reverse", "Bacon Short", "Bacon Short Reverse", "Bacon Kaye", "Illuminati Novice", "English Ordinal", "Reverse Ordinal", "Full Reduction", "Reverse Full Reduction", "Modern Kaye", "Alphanumeric"]
+var openCiphers = ["English Ordinal", "Reverse Ordinal", "Full Reduction", "Reverse Full Reduction", "Bacon Simple", "Bacon Reverse", "Bacon Short", "Bacon Short Reverse", "Bacon Kaye", "Modern Kaye", "Illuminati Novice", "Alphanumeric"]
 var ciphersOn = []; allCiphers = []; sHistory = []
 var opt_NumCalculation = "Full"
 var primeArr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 
@@ -984,7 +984,7 @@ function Add_AllCiphers(impBool = false) {
 }
 function Add_BaseCiphers(impBool = false) {
 	var x, q, cN, z
-	var baseCiphers = ["Bacon Simple", "Bacon Reverse", "Bacon Short", "Bacon Short Reverse", "Bacon Kaye", "Illuminati Novice", "English Ordinal", "Reverse Ordinal", "Full Reduction", "Reverse Full Reduction", "Modern Kaye", "Alphanumeric"]
+	var baseCiphers = ["English Ordinal", "Reverse Ordinal", "Full Reduction", "Reverse Full Reduction", "Bacon Simple", "Bacon Reverse", "Bacon Short", "Bacon Short Reverse", "Bacon Kaye", "Modern Kaye", "Illuminati Novice", "Alphanumeric"]
 
 	openCiphers = []
 	for (z = 0; z < allCiphers.length; z++) {
